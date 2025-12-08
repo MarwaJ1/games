807abb2c6adfc6c4b7f47aab1333c418f4dacef2
Marwa Jarad <marwajarad0@gmail.com>
2025-12-08 18:52:38 +0000

    function that returns radius of either globe, cone or cylinder

---
+++ radius.py
@@ -0,0 +1,31 @@
+#Function that determentns radius of a cone, globe and cylinder by using the height nad volym 
+import math
+
+# Cylinder = 3.14 * r**2 * h = V
+# Cone = 3.14 * r**2 * h / 3 = v
+# Globe = 3.14 * r**3 / 4/3
+def get_radius(v, object, h=0):
+
+    if object.lower() == "cylinder": 
+        temp = v / 3.14 / h 
+        radius = math.sqrt(temp)
+        return radius 
+    
+    elif object.lower() == "cone": 
+        temp = v /3.14 / h * 3
+        radius = math.sqrt(temp)
+        return radius
+    
+    elif object.lower() == "globe": 
+        temp = v / 3.14 / 1.33
+        radius = temp ** (1/3)
+        return radius
+
+# Testing 
+print(get_radius(25.12, "cylinder", 2))
+print(get_radius(8.38, "cone", 2))
+print(get_radius(268, "globe"))
+
+
+    
+
